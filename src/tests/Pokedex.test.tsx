0e26f3a2853c37043edff8de2dac6f2161cdd168
analaurafr <laurafernandes@outlook.com>
2023-09-03 02:07:44 -0300

    começando requisito 5

--- src/tests/Pokedex.test.tsx
+++ src/tests/Pokedex.test.tsx
@@ -1,1 +1,12 @@
-test('', () => {});
+import '@testing-library/jest-dom';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Teste o componente Pokedex', () => {
+  test('Teste se a página contém um heading h2 com o texto Encountered Pokémon', () => {
+    renderWithRouter(<App />);
+    const pageText = screen.getByRole('heading', { name: /encountered pokémon/i });
+    expect(pageText).toBeInTheDocument();
+  });
+});
