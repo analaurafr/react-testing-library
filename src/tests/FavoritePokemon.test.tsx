69cc336d5d62c4cd0e904413374a53a693e76be2
analaurafr <laurafernandes@outlook.com>
2023-09-03 18:35:06 -0300

    requisito favorito

--- src/tests/FavoritePokemon.test.tsx
+++ src/tests/FavoritePokemon.test.tsx
@@ -1,16 +1,47 @@
-import '@testing-library/jest-dom';
 import { screen } from '@testing-library/react';
-import { FavoritePokemon } from '../pages';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
 describe('Teste o componente FavoritePokemon', () => {
+  renderWithRouter(<App />, { route: '/favorites' });
+
   test('Teste se é exibida na tela a mensagem No favorite pokemon found caso a pessoa não tenha Pokémon favorito', () => {
-    renderWithRouter(<FavoritePokemon />);
-    const naoFavs = screen.getByText(/no favorite pokémon found/i);
-    expect(naoFavs).toBeInTheDocument();
+    const temTexto = screen.getByText('No favorite Pokémon found');
+    expect(temTexto).toBeInTheDocument();
   });
 
-  test('Teste se apenas são exibidos os Pokémons favoritados', () => {
-    renderWithRouter(<FavoritePokemon />);
+  test('Teste se são favoritados', async () => {
+    renderWithRouter(<App />, { route: '/' });
+
+    const mais = screen.getByRole('link', { name: /more details/i });
+    await userEvent.click(mais);
+
+    const fav = screen.getByRole('checkbox', { name: /pokémon favoritado/i });
+    await userEvent.click(fav);
+
+    const favPoke = screen.getByRole('link', { name: /favorite pokémon/i });
+    await userEvent.click(favPoke);
+
+    const imgs = screen.queryAllByRole('img');
+    expect(imgs).toHaveLength(2);
+  });
+  test('Teste se são removidos', async () => {
+    renderWithRouter(<App />);
+
+    const mais = screen.getByRole('link', { name: /more details/i });
+    await userEvent.click(mais);
+
+    const fav = screen.getByRole('checkbox', { name: /pokémon favoritado/i });
+    await userEvent.click(fav);
+
+    const favPoke = screen.getByRole('link', { name: /favorite pokémon/i });
+    await userEvent.click(favPoke);
+    await userEvent.click(mais);
+    await userEvent.click(favPoke);
+    await userEvent.click(fav);
+
+    const texto = screen.getByText(/no favorite pokémon found/i);
+    expect(texto).toBeInTheDocument();
   });
 });
