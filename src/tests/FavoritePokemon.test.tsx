9d1bc31a4c0224a89aea86dfd7ce71bac66c2713
analaurafr <laurafernandes@outlook.com>
2023-09-03 01:36:53 -0300

    requisito 4

--- src/tests/FavoritePokemon.test.tsx
+++ src/tests/FavoritePokemon.test.tsx
@@ -1,1 +1,16 @@
-test('', () => {});
+import '@testing-library/jest-dom';
+import { screen } from '@testing-library/react';
+import { FavoritePokemon } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste o componente FavoritePokemon', () => {
+  test('Teste se é exibida na tela a mensagem No favorite pokemon found caso a pessoa não tenha Pokémon favorito', () => {
+    renderWithRouter(<FavoritePokemon />);
+    const naoFavs = screen.getByText(/no favorite pokémon found/i);
+    expect(naoFavs).toBeInTheDocument();
+  });
+
+  test('Teste se apenas são exibidos os Pokémons favoritados', () => {
+    renderWithRouter(<FavoritePokemon />);
+  });
+});
--- src/tests/NotFound.test.tsx
+++ src/tests/NotFound.test.tsx
@@ -1,1 +1,18 @@
-test('', () => {});
+import '@testing-library/jest-dom';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../pages';
+
+describe('Teste o componente NotFound', () => {
+  test('Teste se a página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    const pageNot = screen.getByRole('heading', { name: /Page requested not found/i });
+    expect(pageNot).toBeInTheDocument();
+  });
+
+  test('Teste se a página mostra a imagem com o texto alternativo', () => {
+    renderWithRouter(<NotFound />);
+    const altText = screen.getByRole('img', { name: /clefairy pushing buttons randomly with text i have no idea what i'm doing/i });
+    expect(altText).toBeInTheDocument();
+  });
+});
